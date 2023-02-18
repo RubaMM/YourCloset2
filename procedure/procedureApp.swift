4216bfebc7ba503b246fa4bb7651bfba09296827
Alanoud <alanwdaw@hotmail.com>
2023-02-18 17:47:57 +0300

    Initial Commit

---
+++ procedure/procedureApp.swift
@@ -0,0 +1,17 @@
+//
+//  procedureApp.swift
+//  procedure
+//
+//  Created by alanoud alawwad on 27/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct procedureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
